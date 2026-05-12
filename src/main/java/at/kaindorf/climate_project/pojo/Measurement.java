498f641db9eb59dae075157be4ba20bfa735f77f
gelmad22 <gelmad22@htl-kaindorf.at>
2026-05-12 13:46:23 +0200

    Added Annotations to Measurement.java

--- src/main/java/at/kaindorf/climate_project/pojo/Measurement.java
+++ src/main/java/at/kaindorf/climate_project/pojo/Measurement.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Measurement {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
