3522f44835f58d34e52744a72bfcca82c70f1fb5
gelmad22 <gelmad22@htl-kaindorf.at>
2026-05-12 14:17:52 +0200

    Added run method to MainController.java

--- src/main/java/at/kaindorf/climate_project/MainController.java
+++ src/main/java/at/kaindorf/climate_project/MainController.java
@@ -1,9 +1,15 @@
 package at.kaindorf.climate_project;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
-public class MainController {
+public class MainController implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
