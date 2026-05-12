e25efb437ca71754ab9f6ef6da8edd78f6323a2c
gelmad22 <gelmad22@htl-kaindorf.at>
2026-05-12 13:42:33 +0200

    Implemented map method logic in DtoMapper.java

--- src/main/java/at/kaindorf/climate_project/mapper/DtoMapper.java
+++ src/main/java/at/kaindorf/climate_project/mapper/DtoMapper.java
@@ -1,10 +1,30 @@
 package at.kaindorf.climate_project.mapper;
 
+import at.kaindorf.climate_project.annotations.ToDto;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DtoMapper {
     public static Map<String, Object> map(Object o) {
-        return null;
+        Map<String, Object> dto = new HashMap<>();
+        Field[] fields = o.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            if(field.getAnnotation(ToDto.class) == null){
+                continue;
+            }
+
+            field.setAccessible(true);
+
+            try {
+                dto.put(field.getName(), field.get(o));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return dto;
     }
 }
 
