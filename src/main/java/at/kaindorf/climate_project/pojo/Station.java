1daefafc82cafd30b70c42665674fafd1076eb4d
gelmad22 <gelmad22@htl-kaindorf.at>
2026-05-19 14:45:28 +0200

    Station annotation

--- src/main/java/at/kaindorf/climate_project/pojo/Station.java
+++ src/main/java/at/kaindorf/climate_project/pojo/Station.java
@@ -1,4 +1,14 @@
 package at.kaindorf.climate_project.pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Station {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
 }
+
